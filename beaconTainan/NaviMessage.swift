afb44fff02d8c547fcca4ffe95b1c88c78af45be
AllisonC <102703052@gmail.com>
2018-06-28 21:30:44 +0800

    first commit

---
+++ beaconTainan/NaviMessage.swift
@@ -0,0 +1,9 @@
+//
+//  NaviMessage.swift
+//  beaconTainan
+//
+//  Created by AllisonC on 2018/5/9.
+//  Copyright © 2018年 AllisonC. All rights reserved.
+//
+
+import Foundation
---
+++ beaconTainan/route.swift
@@ -0,0 +1,9 @@
+//
+//  route.swift
+//  beaconTainan
+//
+//  Created by AllisonC on 2018/6/6.
+//  Copyright © 2018年 AllisonC. All rights reserved.
+//
+
+import Foundation
